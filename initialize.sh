e6be0f6283380a8909274f37e91aad8f2bfe9c4f
gooooooooooooogle <1185025336@qq.com>
2021-09-08 09:56:54 +0800

    update

---
+++ initialize.sh
@@ -0,0 +1,18 @@
+# 设置下载源
+npm config set registry=https://registry.npm.taobao.org
+
+# 切换目录
+cd /usr/src
+
+# 下载项目
+# git clone https://github.com/gooooooooooooogle/Express-Restful-Api.git
+git clone https://github.com.cnpmjs.org/gooooooooooooogle/Express-Restful-Api.git
+
+# 切换到项目的根目录
+cd Express-Restful-Api
+
+# 安装依赖包
+npm install
+
+# 启动服务器
+npm run start
